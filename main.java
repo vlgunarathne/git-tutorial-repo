2953adae5c76cb095db6b31f1c2fd72df08e35d1
Viduranga Gunarathne <vidurangagunarathne@Vidurangas-Macbook-Pro.local>
2022-08-27 23:07:16 +0530

    Fix bug in printName

--- main.java
+++ main.java
@@ -3,7 +3,7 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
     }
 
-    public void printName(string name) {
-        System.out.println(name);
+    public void printName(string firstName, string lastName) {
+        System.out.println(firstName + " " + lastName);
     }
 }
