487ed74e499fe1115ed34e21cee0ec45cc5551a6
Viduranga Gunarathne <vidurangagunarathne@Vidurangas-Macbook-Pro.local>
2022-08-27 22:29:58 +0530

    Implement printName function

--- main.java
+++ main.java
@@ -1,5 +1,9 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!"); 
-    } 
+    }
+
+    public void printName(string name) {
+        System.out.println(name);
+    }
 }
